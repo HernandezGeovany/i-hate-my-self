0d261d2059339827f9274e1bb6ddbea9f931a196
HernandezGeovany <94844065+HernandezGeovany@users.noreply.github.com>
2024-01-03 13:58:01 +0100

    Update kys.py

--- kys.py
+++ kys.py
@@ -1,10 +1,11 @@
-
+import yfinance as yf
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock **closing price** and ***volume*** of Google!
+Shown are the stock closing price and volume of Google!
 
 """)
 
@@ -17,11 +18,5 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-st.write("""
-## Closing Price
-""")
 st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
 st.line_chart(tickerDf.Volume)
